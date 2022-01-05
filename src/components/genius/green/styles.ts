fe89994f1a788ade9fe07a11f9a953cdbe791a86
DanRantino <danielaugustofurst@hotmail.com>
2022-01-05 20:50:05 -0300

    v_1

---
+++ src/components/genius/green/styles.ts
@@ -0,0 +1,9 @@
+import styled, { css } from 'styled-components'
+
+export const DivGreen = styled.div`
+  ${() => css`
+    grid-area: verde;
+    background-color: green;
+    border-top-left-radius: 100%;
+  `}
+`
---
+++ src/components/genius/yellow/index.tsx
@@ -0,0 +1,6 @@
+import { FC } from 'react'
+import { DivYellow } from './styles'
+
+export const Yellow: FC = () => {
+  return <DivYellow />
+}
